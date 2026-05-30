00b4b3d5a3e6bb3876b7cf569eb57139e645328a
nghganh217 <nghganh217@gmail.com>
2026-05-31 00:04:30 +0700

    Resolve merge conflicts in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
@@ -12,7 +12,7 @@ function App() {
       <AppRoutes />
 
     </BrowserRouter>
-=======
+
 import "./App.css";
 import RobotAssistantFloating from "./components/ai/RobotAssistantFloating";
 
@@ -21,7 +21,6 @@ function App() {
     <div className="min-h-screen bg-gray-50">
       <RobotAssistantFloating />
     </div>
->>>>>>> main
   );
 }
 
